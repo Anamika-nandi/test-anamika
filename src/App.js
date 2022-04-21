c5d4bcf0ea6b8821f328f68522822ccb476b935a
Anamika-bitcanny <anamika@bitcanny.com>
2022-04-21 16:10:40 +0530

    initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import moment from "moment";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component{
+ state={
+  value:1,
+  lastModified:null,
+ }
+
+ handleIncr=()=>{
+
+   if(this.state.lastModified===null || moment(this.state.lastModified).diff(moment.now(),'seconds')<=-30){
+
+    this.setState(
+      {
+       value:this.state.value+1,
+       lastModified : moment.now(),
+    }
+    );
+
+   }
+ 
+ }
+
+ handleDecr=()=>{
+  this.setState(
+    {
+     value:this.state.value-1,
+  }
   );
+ }
+ render(){
+   return(
+   <>
+   <button onClick={this.handleIncr}>+1</button>  
+   <span>{this.state.value}</span>
+   <button onClick={this.handleDecr}>-1</button>
+   </>)
+ }
 }
 
 export default App;
